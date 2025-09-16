be81f5fec0588da6d54a738fd05f83b7794d8f77
Volmer42 <77463801+VolmerES@users.noreply.github.com>
2025-09-16 18:16:42 +0200

    add: Robotomy Class hpp

---
+++ Module05/ex02/inc/RobotomyRequestForm.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RobotomyRequestForm.hpp                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: volmer <volmer@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/16 18:12:22 by volmer            #+#    #+#             */
+/*   Updated: 2025/09/16 18:15:56 by volmer           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "AForm.hpp"
+
+class RobotomyRequestForm : public AForm {
+	private:
+		const std::string _target;
+	public:
+		RobotomyRequestForm(const std::string &target);
+		RobotomyRequestForm(const RobotomyRequestForm& other);
+		RobotomyRequestForm& operator=(const RobotomyRequestForm& other);
+		virtual ~RobotomyRequestForm();
+
+		virtual void	execute(Bureaucrat const & executor) const;
+};
