77139b787267bda05e82c57eaa62b4e8ae6a3384
Volmer42 <77463801+VolmerES@users.noreply.github.com>
2025-09-25 14:07:04 +0200

    fix: update main function to validate argument count and display usage instructions

--- Module06/ex00/src/main.cpp
+++ Module06/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: volmer <volmer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 14:44:31 by volmer            #+#    #+#             */
-/*   Updated: 2025/09/25 14:03:16 by volmer           ###   ########.fr       */
+/*   Updated: 2025/09/25 14:06:27 by volmer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 
 int	main(int argc, char **argv) {
 
-	(void)argc;
-	std::cout << "Usage: ./convert \"value\"" << std::endl;
-
+	if (argc != 2){
+		std::cout << "Invalid Format" << std::endl;
+		std::cout << "Usage: ./convert \"value\"" << std::endl;
+		return(1);
+	}
 	ScalarConverter::convert(argv[1]);
 	return (0);
 }
