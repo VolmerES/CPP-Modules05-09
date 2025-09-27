d24c962d05900b95070fda1555c7534844680f14
Juan Bautista Delorme Pinedo <jdelorme@c1r1s3.42madrid.com>
2025-09-27 11:43:39 +0200

    fix commments

--- Module05/ex03/src/ShrubberyCreationForm.cpp
+++ Module05/ex03/src/ShrubberyCreationForm.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ShrubberyCreationForm.cpp                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: volmer <volmer@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jdelorme <jdelorme@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 18:25:35 by volmer            #+#    #+#             */
-/*   Updated: 2025/09/18 11:13:41 by volmer           ###   ########.fr       */
+/*   Updated: 2025/09/27 11:42:29 by jdelorme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,23 +34,15 @@ ShrubberyCreationForm& ShrubberyCreationForm::operator=(const ShrubberyCreationF
 ShrubberyCreationForm::~ShrubberyCreationForm() {}
 
 void ShrubberyCreationForm::execute(Bureaucrat const& executor) const {
-    // 1) Verificar que el formulario está firmado y que el executor tiene el rango suficiente
-    //    (usa checkExecutable(executor))
+
 	checkExecutable(executor);
 
-    // 2) Construir el nombre del archivo: "<target>_shrubbery"
 	std::string name = _target + "_shrubbery";
-
-    // 3) Abrir un ofstream con ese nombre
 	std::ofstream target(name);
 
-    // 4) Comprobar que el archivo se ha abierto correctamente
-    //    -> si no, lanzar una excepción (std::runtime_error por ejemplo)
 	if (!target.is_open()) {
 		throw std::runtime_error("No se pudo abrir el archivo");
 	}
-	
-    // 5) Escribir un par de árboles ASCII en el archivo
 	target <<
 	"         ,%%%,%%%%%%%\n"
 	"       ,'%% \\-*%%%%%%%\n"
@@ -62,6 +54,5 @@ void ShrubberyCreationForm::execute(Bureaucrat const& executor) const {
 	"             _.) ,/ *%,\n"
 	"     _________/)#(_____________\n";
 
-    // 6) Cerrar el archivo
 	target.close();
 }
