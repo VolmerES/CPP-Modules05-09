8c409cedf3cffcea686e2160a18ab385dbc10baf
Volmer42 <77463801+VolmerES@users.noreply.github.com>
2025-09-29 14:03:40 +0200

    feat: implement max, min, and swap functions in whatever.hpp and update main.cpp

--- Module07/ex00/inc/whatever.hpp
+++ Module07/ex00/inc/whatever.hpp
@@ -1,20 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   min.cpp                                            :+:      :+:    :+:   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: volmer <volmer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/29 13:54:36 by volmer            #+#    #+#             */
-/*   Updated: 2025/09/29 13:58:33 by volmer           ###   ########.fr       */
+/*   Created: 2025/09/29 14:02:06 by volmer            #+#    #+#             */
+/*   Updated: 2025/09/29 14:02:46 by volmer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-template <typename type>
+#pragma once
 
+template <typename type>
+type	max(type a, type b)
+{
+	if (a > b)	return(a);
+	if (b > a)	return(b);
+	else		return(b);
+}
+
+template <typename type>
 type	min(type a, type b)
 {
 	if (a > b)	return(b);
 	if (b > a)	return(a);
 	else		return(b);
+}
+
+template <typename Type1>
+
+void	swap(Type1 a, Type1 b) 
+{
+	Type1 swap;
+
+	swap = a;
+	a = b;
+	b = swap;
 }
---
+++ Module07/ex00/src/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: volmer <volmer@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/29 13:59:19 by volmer            #+#    #+#             */
+/*   Updated: 2025/09/29 14:03:09 by volmer           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "../inc/whatever.hpp"
+
+int main( void ) {
+int a = 2;
+int b = 3;
+::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
