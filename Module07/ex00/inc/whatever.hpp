a63273c3cd2280869f16af0038aebfd1e754682b
Volmer42 <77463801+VolmerES@users.noreply.github.com>
2025-09-29 14:07:07 +0200

    fix: correct object file path in Makefile and update swap function parameter to use references

--- Module07/ex00/inc/whatever.hpp
+++ Module07/ex00/inc/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: volmer <volmer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 14:02:06 by volmer            #+#    #+#             */
-/*   Updated: 2025/09/29 14:02:46 by volmer           ###   ########.fr       */
+/*   Updated: 2025/09/29 14:06:33 by volmer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,7 @@ type	min(type a, type b)
 }
 
 template <typename Type1>
-
-void	swap(Type1 a, Type1 b) 
+void	swap(Type1& a, Type1& b) 
 {
 	Type1 swap;
 
