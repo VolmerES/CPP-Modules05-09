6a6929dfae5c893a270a21e0e020a92cb065693d
Volmer42 <77463801+VolmerES@users.noreply.github.com>
2025-10-07 18:32:18 +0200

    fix: update Span constructor to reserve data and enhance addNumber method with error handling

--- Module08/ex01/src/Span.cpp
+++ Module08/ex01/src/Span.cpp
@@ -6,13 +6,17 @@
 /*   By: volmer <volmer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 17:04:23 by volmer            #+#    #+#             */
-/*   Updated: 2025/10/07 17:56:04 by volmer           ###   ########.fr       */
+/*   Updated: 2025/10/07 18:28:00 by volmer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/Span.hpp"
+#include <stdexcept>
+#include <vector>
 
-Span::Span(unsigned int max) : _max(max) {}
+Span::Span(unsigned int max) : _max(max) {
+	_data.reserve(max);
+}
 
 Span::Span(const Span & other) : _max(other._max), _data(other._data) {}
 
@@ -28,8 +32,27 @@ Span& Span::operator=(const Span & other)
 
 Span::~Span() {}
 
-void	addNumber(int nb)
+void	Span::addNumber(int nb)
 {
-	//will be used in order to fill it. Any attempt to add a new element if there.
-	//are already N elements stored should throw an exception.
+	if (_data.size() <= _max)
+		throw std::runtime_error("Cannot add more numbers, Span in full");
+	_data.push_back(nb);
+}
+template<typename T>
+void			Span::addRange(T first, T last)
+{
+	
+}
+
+unsigned long	Span::shortestSpan() const
+{
+	if (_data.size() <= 2)
+		throw std::runtime_error("Not enough numbers");
+	std::vector<int> sortedata = _data;
+	std::sort(sortedata.begin(), sortedata.end());
+}
+
+unsigned long	Span::longestSpan() const
+{
+	
 }
