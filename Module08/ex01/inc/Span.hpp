aa30c5653d55db4d0f4f69dfa9a3231d8c9f4dc2
Volmer42 <77463801+VolmerES@users.noreply.github.com>
2025-10-07 17:56:15 +0200

    feat: add copy constructor and assignment operator to Span class, and implement addRange template method

--- Module08/ex01/inc/Span.hpp
+++ Module08/ex01/inc/Span.hpp
@@ -6,20 +6,26 @@
 /*   By: volmer <volmer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 16:55:01 by volmer            #+#    #+#             */
-/*   Updated: 2025/10/07 17:10:44 by volmer           ###   ########.fr       */
+/*   Updated: 2025/10/07 17:53:08 by volmer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
 class Span {
+	private:
+		unsigned int _max;
+    	std::vector<int> _data;
 	public:
 		Span(unsigned int max);
 		Span(const Span & other);
 		Span& operator=(const Span & other);
 		~Span();
 
-		void	addNumber(int nb);
-		void	shortestSpan();
-		void	longestSpan();
+		template<typename T>
+		void			addRange(T first, T last);
+
+		void			addNumber(int nb);
+		unsigned long	shortestSpan() const;
+		unsigned long	longestSpan() const;
 };
--- Module08/ex01/src/Span.cpp
+++ Module08/ex01/src/Span.cpp
@@ -6,27 +6,30 @@
 /*   By: volmer <volmer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 17:04:23 by volmer            #+#    #+#             */
-/*   Updated: 2025/10/07 17:08:09 by volmer           ###   ########.fr       */
+/*   Updated: 2025/10/07 17:56:04 by volmer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/Span.hpp"
 
-Span::Span(unsigned int max) {
-	
-}
+Span::Span(unsigned int max) : _max(max) {}
 
-Span::Span(const Span & other) {
-	
-}
+Span::Span(const Span & other) : _max(other._max), _data(other._data) {}
 
-Span& Span::operator=(const Span & other) {
-
+Span& Span::operator=(const Span & other) 
+{
+    if (this != &other)
+	{
+        _max = other._max;
+        _data = other._data;
+    }
+    return (*this);
 }
 
 Span::~Span() {}
 
-void	addNumber(int nb) {
-	//will be used in order to fill it. Any attempt to add a new element if there
-//are already N elements stored should throw an exception.
-}	
+void	addNumber(int nb)
+{
+	//will be used in order to fill it. Any attempt to add a new element if there.
+	//are already N elements stored should throw an exception.
+}
