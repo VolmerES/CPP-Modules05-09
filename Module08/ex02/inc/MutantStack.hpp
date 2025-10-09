43301e536e8621d2f4793715916e1e2bb8147674
Volmer42 <77463801+VolmerES@users.noreply.github.com>
2025-10-09 16:04:33 +0200

    feat: add MutantStack class implementation with main function and Makefile setup

---
+++ Module08/ex02/inc/MutantStack.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: volmer <volmer@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/09 15:05:50 by volmer            #+#    #+#             */
+/*   Updated: 2025/10/09 16:03:16 by volmer           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <stack>
+#include <deque>
+
+template <typename T>
+class MutantStack : public std::stack<T> {
+	public:
+		MutantStack();
+		MutantStack(const MutantStack & other);
+		MutantStack& 	operator=(const MutantStack & other);
+		~MutantStack();
+
+		typedef typename std::stack<T>::container_type::iterator it;
+		typedef typename std::stack<T>::container_type::const_iterator const_it;
+		
+		it			begin;
+		it			end;
+		const_it	begin;
+		const_it	end;
+};
+
