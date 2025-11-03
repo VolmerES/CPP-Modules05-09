6f4ed751522efeeda2100e7e98569f795ea4d490
Volmer42 <77463801+VolmerES@users.noreply.github.com>
2025-11-03 18:10:45 +0100

    Refactor BitcoinExchange class: rename _database to _rates, add private methods for rate retrieval and input processing, and update comments for clarity.

--- Module09/ex00/inc/BitcoinExchange.hpp
+++ Module09/ex00/inc/BitcoinExchange.hpp
@@ -6,21 +6,30 @@
 /*   By: volmer <volmer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 13:37:50 by volmer            #+#    #+#             */
-/*   Updated: 2025/11/03 15:39:44 by volmer           ###   ########.fr       */
+/*   Updated: 2025/11/03 18:09:56 by volmer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+#include <string>
 #include <map>
 
 class BitcoinExchange {
 	private:
-		std::map<std::string, double> _database;
+		std::map<std::string, double> _rates;
+		
+		double		getRateForDate(const std::string & date) const;
+		static bool	isValidDate(const std::string & date);
+		static bool	processInputFile(const std::string & line,
+                               std::string & date, double& value);
 	public:
 		BitcoinExchange();
 		BitcoinExchange(const BitcoinExchange & other);
 		BitcoinExchange& operator=(const BitcoinExchange & other);
 		~BitcoinExchange();
+
+		void		loadDatabase(const std::string & datapath);
+		void		handleInput(const std::string & inputpath) const;
 };
