7de1c28ee9b56f1bf6c3b992c392b589a758f9ce
Volmer42 <77463801+VolmerES@users.noreply.github.com>
2025-11-05 16:18:09 +0100

    feat: add BitcoinExchange class and implement main function for input handling

--- Module09/ex00/src/main.cpp
+++ Module09/ex00/src/main.cpp
@@ -6,10 +6,24 @@
 /*   By: volmer <volmer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 13:39:48 by volmer            #+#    #+#             */
-/*   Updated: 2025/11/03 13:40:06 by volmer           ###   ########.fr       */
+/*   Updated: 2025/11/05 16:13:01 by volmer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
  #include "../inc/BitcoinExchange.hpp"
  
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cerr << "Error: Not enough arguments" << std::endl;
+		return (1);
+	}
 
+	BitcoinExchange BTC;
+	
+	BTC.loadDatabase("data.csv");
+	BTC.handleInput(argv[1]);
+
+	return (0);
+}
